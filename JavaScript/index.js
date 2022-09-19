534f8ebd17f69e0f657cb3b6bae9bcb1370ea2d4
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-09-19 18:41:46 +0530

    added image slider and some bugs fixes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,57 +1,45 @@
-const menu = document.querySelector(".menu");
-const menuMain = menu.querySelector(".menu-main");
-const goBack = menu.querySelector(".go-back");
-const menuTrigger = document.querySelector(".layer");
-const closeMenu = menu.querySelector(".mobile-menu-close");
-let subMenu;
-menuMain.addEventListener("click", (e) => {
-  if (!menu.classList.contains("active")) {
-    return;
-  }
-  if (e.target.closest(".menu-item-has-children")) {
-    const hasChildren = e.target.closest(".menu-item-has-children");
-    showSubMenu(hasChildren);
-  }
-});
-goBack.addEventListener("click", () => {
-  hideSubMenu();
-});
-menuTrigger.addEventListener("click", () => {
-  toggleMenu();
-});
-closeMenu.addEventListener("click", () => {
-  toggleMenu();
-});
-document.querySelector(".menu-overlay").addEventListener("click", () => {
-  toggleMenu();
-});
-function toggleMenu() {
-  menu.classList.toggle("active");
-  document.querySelector(".menu-overlay").classList.toggle("active");
-}
-function showSubMenu(hasChildren) {
-  subMenu = hasChildren.querySelector(".sub-menu");
-  subMenu.classList.add("active");
-  subMenu.style.animation = "slideLeft 0.5s ease forwards";
-  const menuTitle =
-    hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
-  menu.querySelector(".current-menu-title").innerHTML = menuTitle;
-  menu.querySelector(".mobile-menu-head").classList.add("active");
-}
 
-function hideSubMenu() {
-  subMenu.style.animation = "slideRight 0.5s ease forwards";
-  setTimeout(() => {
-    subMenu.classList.remove("active");
-  }, 300);
-  menu.querySelector(".current-menu-title").innerHTML = "";
-  menu.querySelector(".mobile-menu-head").classList.remove("active");
-}
-
-window.onresize = function () {
-  if (this.innerWidth > 799) {
-    if (menu.classList.contains("active")) {
-      toggleMenu();
+// SLIDER IMAGE CODE START
+var slideIndex = 0;
+showSlides();
+var slides;
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
     }
-  }
-};
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    
+    slides[slideIndex-1].style.display = "block";  
+   
+}
+
+function currentSlide(index) {
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+   
+    slides[index-1].style.display = "block";  
+}
+// SLIDER IMAGE CODE END
+
+//GET SLIDER IMAGE FROM FIREBASE REALTIME DATABASE (START)
+
+//GET SLIDER IMAGE FROM FIREBASE REALTIME DATABASE (END)
